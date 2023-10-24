19e0ab5ae2bdb1e0ba01ce9ae825ccd1aab9034c
CHAPELLE Quentin <quentinchap@users.noreply.github.com>
2023-10-24 23:06:42 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,9 +9,9 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
+   
         My Beer Factory
-      </Link>{" "}
+    {" "}
       {new Date().getFullYear()}
       {"."}
     </Typography>
